cf1284843cad798fa5c203964ff81986ad145646
Yakup Ekinci <yakup_metin@hotmail.com>
2023-11-11 03:25:01 +0300

    Terrain Test

---
+++ DeadlyWayInUniverses/Assets/MyAssetsFolder/Scripts/PlayersMovement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class PlayersMovement : MonoBehaviour
+{
+    public float movementSpeed = 5.0f;
+    public float mouseSensitivity = 2.0f;
+
+    private Camera freeLookCam;
+    private float verticalRotation = 0f;
+
+    void Start()
+    {
+        freeLookCam = FindObjectOfType<Camera>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        // Hareket kontrolleri
+        float horizontalMovement = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
+        float verticalMovement = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
+
+        Vector3 movement = transform.TransformDirection(new Vector3(horizontalMovement, 0, verticalMovement));
+        transform.Translate(movement);
+
+        // Fare hareketi ile kamera dönüşü
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = -Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        verticalRotation += mouseY;
+        verticalRotation = Mathf.Clamp(verticalRotation, -90f, 90f);
+
+    
+
+        // Oyuncunun yatay dönüşü
+        transform.rotation *= Quaternion.Euler(0, mouseX, 0);
+    }
+}
