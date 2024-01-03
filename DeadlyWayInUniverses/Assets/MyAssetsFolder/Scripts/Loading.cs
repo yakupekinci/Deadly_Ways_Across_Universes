6073b18153717909106b116f1f9961379d1bd0ff
Yakup Ekinci <yakup_metin@hotmail.com>
2024-01-03 21:05:13 +0300

    CAVE & UI

---
+++ DeadlyWayInUniverses/Assets/MyAssetsFolder/Scripts/Loading.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Loading : MonoBehaviour
+{
+    [SerializeField] private GameObject LoadingScrren;
+    [SerializeField] private Slider loaddingBarFill;
+    [SerializeField] private GameObject uıCamera;
+
+    public void LoadScene(int sceneId)
+    {
+        StartCoroutine(LoadSceneAsync(sceneId));
+    }
+    public void ActivePanel()
+    {
+        LoadingScrren.gameObject.SetActive(true);
+        loaddingBarFill.gameObject.SetActive(true);
+        uıCamera.SetActive(true);
+    }
+
+    IEnumerator LoadSceneAsync(int sceneId)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneId);
+
+        while (!operation.isDone)
+        {
+            float progressValue = Mathf.Clamp01(operation.progress / 0.01f);
+            loaddingBarFill.value = Mathf.Lerp(loaddingBarFill.value, progressValue, Time.deltaTime * 5f); // Adjust the multiplier for the speed
+
+            yield return null;
+        }
+
+    }
+}
