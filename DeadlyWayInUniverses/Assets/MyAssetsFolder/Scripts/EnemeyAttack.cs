9c8a892a082510c67ae6733d2e24c619d6a8fcb1
Yakup Ekinci <99822215+yakupekinci@users.noreply.github.com>
2024-01-27 21:23:23 +0300

    FirstLizardEnemy movemenet oky

---
+++ DeadlyWayInUniverses/Assets/MyAssetsFolder/Scripts/EnemeyAttack.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemeyAttack : MonoBehaviour
+{
+
+    public void AttackToPlayer()
+    {
+        Debug.Log("ATTACKED");
+    }
+}
--- DeadlyWayInUniverses/Assets/MyAssetsFolder/Scripts/EnemyMovement.cs
+++ DeadlyWayInUniverses/Assets/MyAssetsFolder/Scripts/EnemyMovement.cs
@@ -9,69 +9,118 @@ public class EnemyMovement : MonoBehaviour
     [SerializeField] private float movementSpeed = 15f;
     [SerializeField] private float attackDistance = 5f;
     [SerializeField] private float followDistance = 200f;
+    [SerializeField] private float speedDistance = 20f;
+    [SerializeField] private float jumpHeightThreshold = 1.5f;
+    [SerializeField] private float idleDuration = 2f; // Bekleme süresi
+    [SerializeField] private float randomAreaRadius = 10f; // Rastgele alan yarıçapı
 
     private GameObject player;
     private NavMeshAgent navMeshAgent;
     private Animator anim;
     private bool isActive;
+    private bool isIdle;
+    private float idleTimer;
 
     private void Awake()
     {
-
         anim = transform.GetChild(0).GetComponent<Animator>();
         navMeshAgent = GetComponent<NavMeshAgent>();
         player = FindObjectOfType<PlayerController>().gameObject;
     }
+
     private void Start()
     {
         isActive = true;
         navMeshAgent.SetDestination(player.transform.position);
         navMeshAgent.speed = movementSpeed;
+
+        isIdle = false;
+        idleTimer = 0f;
     }
 
     private void Update()
     {
         if (!isActive)
             return;
-        else
+
+        float distance = Vector3.Distance(transform.position, player.transform.position);
+
+        if (distance < followDistance)
         {
-            float distance = Vector3.Distance(transform.position, player.transform.position);
+            navMeshAgent.SetDestination(player.transform.position);
 
-            if (distance < followDistance)
+            if (distance > speedDistance)
             {
-                navMeshAgent.SetDestination(player.transform.position);
-                if (distance > 20)
-                {
-                    anim.SetBool("isWalking", true); 
-                    anim.SetBool("isRunning", true);
-                    navMeshAgent.speed = movementSpeed * 1.5f;
-                }
-                else if (distance < 20)
-                {
-                    anim.SetBool("isRunning", false);
-                    anim.SetBool("isWalking", true);
-                    navMeshAgent.speed = movementSpeed;
-                }
+                anim.SetBool("isRunning", true);
+                anim.SetBool("isWalking", false);
+                navMeshAgent.speed = movementSpeed * 2f;
+            }
+            else if (distance < speedDistance)
+            {
+                anim.SetBool("isRunning", false);
+                anim.SetBool("isWalking", true);
+                navMeshAgent.speed = movementSpeed;
+            }
 
-                if (distance < attackDistance)
-                {
-                    anim.SetBool("isWalking", false);
-                    anim.SetBool("isAttacking", true);
-                    navMeshAgent.isStopped = true;
-                }
-                else
-                {
-                    anim.SetBool("isWalking", false);
-                    anim.SetBool("isAttacking", false);
-                    navMeshAgent.isStopped = false;
-                }
+            if (distance < attackDistance)
+            {
+                anim.SetBool("isWalking", false);
+                anim.SetBool("isAttacking", true);
+                navMeshAgent.isStopped = true;
             }
             else
             {
-                navMeshAgent.isStopped = true;
+                anim.SetBool("isAttacking", false);
+                navMeshAgent.isStopped = false;
             }
 
+            CheckForJump();
+        }
+        else
+        {
+            if (!isIdle)
+            {
+                SetRandomDestination();
+            }
+            else
+            {
+                idleTimer += Time.deltaTime;
+                if (idleTimer >= idleDuration)
+                {
+                    isIdle = false;
+                    idleTimer = 0f;
+                }
+            }
+        }
+    }
+
+    private void SetRandomDestination()
+    {
+        Vector3 randomDirection = Random.insideUnitSphere * randomAreaRadius;
+        randomDirection += transform.position;
+
+        NavMeshHit hit;
+        NavMesh.SamplePosition(randomDirection, out hit, randomAreaRadius, 1);
+
+        if (hit.position != null)
+        {
+            navMeshAgent.SetDestination(hit.position);
+            anim.SetBool("isWalking", true);
+            isIdle = true;
+        }
+    }
+
+    private void CheckForJump()
+    {
+        float heightDifference = Mathf.Abs(transform.position.y - player.transform.position.y);
+
+        if (heightDifference > jumpHeightThreshold)
+        {
+            anim.SetBool("isJumping", true);
+        }
+        else
+        {
+            anim.SetBool("isJumping", false);
         }
     }
 }
-
