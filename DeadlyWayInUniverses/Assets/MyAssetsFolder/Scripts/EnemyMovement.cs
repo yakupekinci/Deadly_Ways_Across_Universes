cc418a4c97b18c23d2893ff513b38ea69e1cf290
Yakup Ekinci <99822215+yakupekinci@users.noreply.github.com>
2024-01-27 18:50:11 +0300

    Monster

---
+++ DeadlyWayInUniverses/Assets/MyAssetsFolder/Scripts/EnemyMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed = 10f;
+    [SerializeField] private NavMeshAgent navMeshAgent;
+
+    private Animator anim;
+
+    
+    
+
+
+
+
+}
