3a2c4defd22ed92b6117726269ffb75c7034ee41
Yakup Ekinci <99822215+yakupekinci@users.noreply.github.com>
2024-03-06 14:21:29 +0300

    Space Rooms

---
+++ DeadlyWayInUniverses/Assets/Numbers/Digital Keypad System/PackageAssets/Scripts/CanvasInteract.cs
@@ -0,0 +1,20 @@
+﻿//Script written by Matthew Rukas - Volumetric Games || volumetricgames@gmail.com || www.volumetric-games.com
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CanvasInteract : MonoBehaviour
+{
+    [SerializeField] private Canvas keyPadCanvas;
+
+    public void CanvasOn()
+    {
+        keyPadCanvas.enabled = true;
+    }
+
+    public void CanvasOff()
+    {
+        keyPadCanvas.enabled = false;
+    }
+}
---
+++ DeadlyWayInUniverses/Assets/Numbers/Digital Keypad System/PackageAssets/Scripts/OpenDoor.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenDoor : MonoBehaviour
+{
+    [SerializeField] private float smooth = 3.0f;
+    [SerializeField] private int DoorOpenAngle = -90;
+    public bool open;
+
+    private Vector3 defaultRot;
+    private Vector3 openRot;
+
+    void Start()
+    {
+        defaultRot = transform.eulerAngles;
+        openRot = new Vector3(defaultRot.x, defaultRot.y + DoorOpenAngle, defaultRot.z);
+    }
+
+    void Update()
+    {
+        if (open)
+        {
+            //Open door
+            transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, openRot, Time.deltaTime * smooth);
+        }
+
+        else if (!open)
+        {
+            //Close door
+            transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, defaultRot, Time.deltaTime * smooth);
+        }
+    }
+}
